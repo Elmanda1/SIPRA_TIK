b6e337cab99cee938f3e7b36b370ac92708ac5ff
Aurakha <aurakharere@gmail.com>
2025-06-04 20:20:08 +0700

    Penambahan login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './login.jsx' // ubah import ke Login
+import Login from './auth/login.jsx' // ubah import ke Login
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
